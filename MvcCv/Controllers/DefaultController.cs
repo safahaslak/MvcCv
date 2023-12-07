409e2d0425634a631d5d5a820a7538c7dae204cb
safahaslak <haslaksafa@gmail.com>
2023-12-07 16:52:00 +0300

    Add project files.

---
+++ MvcCv/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity; // bu library i eklemezsek veritabanını kullanamayız.
+
+namespace MvcCv.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbCvEntities db = new DbCvEntities(); // db nesnesini ekledik
+        public ActionResult Index()
+        {
+            var degerler = db.TblHakkimdas.ToList();
+            return View(degerler);
+        }
+    }
+}
